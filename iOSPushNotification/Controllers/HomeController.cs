5bf346cbf3060a5605a55ac65f86f096952e3132
nikeeta26@gmail.com <nikeeta26@gmail.com>
2019-04-16 21:00:32 +0530

    push with image

--- iOSPushNotification/Controllers/HomeController.cs
+++ iOSPushNotification/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using iOSPushNotification.Models;
+using Project.MoonAPNS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,18 +15,96 @@ namespace iOSPushNotification.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        [HttpPost]
+        [ValidateAntiForgeryToken]
 
-            return View();
+        public ActionResult Index(Notification obType)
+        {
+            string strResult = string.Empty;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    try
+                    {
+
+                        // int result = 0;
+                        //String[] arrayString;
+                        //string[] arrMerchantId = MerchantIds.Split(',');
+                        string strPushMessage = obType.message;
+                        var appDomain = System.AppDomain.CurrentDomain;
+                        var basePath = appDomain.BaseDirectory;
+                        PushNotification objNo = new PushNotification(true, basePath + "\\jainPanchang_Dev_Push.p12", "1");
+                        List<NotificationPayload> p = new List<NotificationPayload>();
+                        //  var SplitMessageArray = strPushMessage.Split(new char[] { ':' }, 2); ;
+                        //string splitMessage = SplitMessageArray[0].ToString();
+                        List<string> strDeviceIds = new List<string>();
+                        //for (int i = 0; i < arrMerchantId.Length; i++)
+                        //{
+                        //    strDeviceIds = GetDeviceIds(MerchantIds, splitMessage);
+                        //}
+                        //if (strDeviceIds.Count > 0)
+                        //{
+                        //    for (int j = 0; j < strDeviceIds.Count; j++)
+                        //    {
+                        //        switch (splitMessage)
+                        //        {
+                        //            case "Block":
+                        //                {
+                        //                    var regex = new Regex(Regex.Escape("Block:"));
+                        //                    var newText = regex.Replace(strPushMessage, "", 1);
+                        //                    p.Add(new NotificationPayload(strDeviceIds[j].ToString(), newText, 0, "default", "Block"));
+                        //                    break;
+                        //                }
+                        //            case "CSynch":
+                        //                {
+
+                        //                    var regex2 = new Regex(Regex.Escape("CSynch:"));
+                        //                    var newText2 = regex2.Replace(strPushMessage, "", 1);
+                        //                    p.Add(new NotificationPayload(strDeviceIds[j].ToString(), newText2, 0, "default", "CSynch"));
+                        //                    break;
+                        //                }
+                        //            default:
+                        //                {
+                        //                    p.Add(new NotificationPayload(strDeviceIds[j].ToString(), strPushMessage, 0, "default", "DSynch"));
+                        //                    break;
+                        //                }
+                        //        }
+
+
+                        //    }
+
+                        var payload1 = new NotificationPayload(obType.tokenId, strPushMessage, 10, "default","", "http://hd-wall-papers.com/images/wallpapers/animal-nature-wallpaper/animal-nature-wallpaper-7.jpg");
+                        //var payload2 = new NotificationPayload("610f02246c62f99d2b92a7610a7ce2215574fa9139292e7a98b428961b924261", strPushMessage, 12, "default");
+                        p.Add(payload1);
+                        //p.Add(payload2);
+
+
+                        string certificatePath = basePath + "\\jainPanchang_Dev_Push.p12";
+                        var push = new PushNotification(true, certificatePath, "1");
+                        string strfilename = push.P12File;
+                        var message1 = push.SendToApple(p);
+                        //foreach (var item in message1)
+                        //{
+                        //    result = 1;
+                        //}
+                        strResult = "Success";
+
+                    }
+                    catch (Exception ef)
+                    {
+                        strResult = "fail";
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            ViewBag.message = strResult;
+            return View(obType);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
